1d091676c0ee2e810289d4fdaec85274929e92b5
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-01 18:36:23 -0700

    Registerd connection string with Startup.cs

--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AsyncInn.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,11 +15,23 @@ namespace AsyncInn
 {
     public class Startup
     {
+        // Property to enable DI for config file.
+        public IConfiguration Configuration { get; }
+
+        // Ctor to enable DI for config file with Configuration prop.
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         
         public void ConfigureServices(IServiceCollection services)
         {
             // Tell our application to use MVC.
             services.AddMvc();
+
+            // Tell our app to use our default connection string from app settings file.
+            services.AddDbContext<AsyncInnDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
