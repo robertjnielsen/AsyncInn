201ede5c32c27140e3304ebc6fa2aa08d952b444
harlen lopez <harlen.d.lopez@gmail.com>
2020-04-02 19:17:42 -0700

    model builder key

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,11 @@ namespace AsyncInn.Data
         public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
         {
             
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HotelRoom>().HasKey(e => new { e.HotelID, e.RoomNumber });
+            modelBuilder.Entity<RoomAmenities>().HasKey(e => new { e.RoomID, e.AmenitiesID });
         }
     }
 }
