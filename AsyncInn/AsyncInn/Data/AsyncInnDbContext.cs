3bd43af384f447b98aa5ec318df897e8bd63c9af
harlen lopez <harlen.d.lopez@gmail.com>
2020-04-02 19:26:00 -0700

    created tables from our models

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+/// <summary>
+/// 
+/// </summary>
 namespace AsyncInn.Data
 {
     public class AsyncInnDbContext : DbContext
@@ -13,10 +15,23 @@ namespace AsyncInn.Data
         {
             
         }
+        /// <summary>
+        /// creates composite keys for join tables
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<HotelRoom>().HasKey(e => new { e.HotelID, e.RoomNumber });
             modelBuilder.Entity<RoomAmenities>().HasKey(e => new { e.RoomID, e.AmenitiesID });
         }
+        /// <summary>
+        /// Creates at table for each model
+        /// </summary>
+        public DbSet<Amenities> Amenities { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
+
     }
 }
