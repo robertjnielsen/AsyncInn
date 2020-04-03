5dd7ec041352a6555bdcfa019afd58a771019a84
harlen lopez <harlen.d.lopez@gmail.com>
2020-04-02 18:52:42 -0700

    models added

--- AsyncInn/AsyncInn/Models/Hotel.cs
+++ AsyncInn/AsyncInn/Models/Hotel.cs
@@ -7,5 +7,14 @@ namespace AsyncInn.Models
 {
     public class Hotel
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        //Navigation Properties
+        public List<HotelRoom> HotelRooms { get; set; }
+
     }
 }
