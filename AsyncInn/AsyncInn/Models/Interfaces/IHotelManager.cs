100150fe8e76f851b52c0d2e64fa75df404ace20
harlen lopez <harlen.d.lopez@gmail.com>
2020-04-07 18:46:55 -0700

    interfaces added

---
+++ AsyncInn/AsyncInn/Models/Interfaces/IHotelManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    public interface IHotelManager
+    {
+        Task<Hotel> CreateHotel(Hotel hotel);
+        Task UpdateHotel(int HotelID, Hotel hotel);
+        Task<List<Hotel>> GetAllHotels();
+        Task<Hotel> GetHotelByID(int HotelID);
+        Task RemoveHotel(int HotelID);
+    }
+
+}
