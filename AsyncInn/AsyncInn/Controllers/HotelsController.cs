e6c730c177dea2540da9e9a80a228e4cb7445800
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-08 13:32:58 -0700

    HotelsController updated.

--- AsyncInn/AsyncInn/Controllers/HotelsController.cs
+++ AsyncInn/AsyncInn/Controllers/HotelsController.cs
@@ -15,13 +15,38 @@ namespace AsyncInn.Controllers
     [ApiController]
     public class HotelsController : ControllerBase
     {
+        /// <summary>
+        /// Assigns the DB table / entity via IHotelManager and DI.
+        /// </summary>
         private readonly IHotelManager _hotel;
 
+        /// <summary>
+        /// Constructor method for the controller.
+        /// </summary>
+        /// <param name="hotel">The Hotels table / entity passed by IHotelManager.</param>
         public HotelsController(IHotelManager hotel)
         {
             _hotel = hotel;
         }
 
+        /// <summary>
+        /// Adds a new Hotel object to the Hotels table / entity.
+        /// </summary>
+        /// <param name="hotel">The Hotel object to insert into the table.</param>
+        /// <returns>The newly created Hotel object.</returns>
+        // POST: api/Hotels
+        [HttpPost]
+        public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
+        {
+            var result = await _hotel.CreateHotel(hotel);
+
+            return CreatedAtAction("GetHotel", new { id = result.ID }, result);
+        }
+
+        /// <summary>
+        /// Gets all Hotel objects from the Hotels table.
+        /// </summary>
+        /// <returns>Returns a list of all Hotel objects.</returns>
         // GET: api/Hotels
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Hotel>>> GetHotels()
@@ -29,77 +54,57 @@ namespace AsyncInn.Controllers
             return await _hotel.GetAllHotels();
         }
 
-        /*// GET: api/Hotels/5
+        /// <summary>
+        /// Gets a single Hotel object from the Hotels table.
+        /// </summary>
+        /// <param name="id">The ID of the Hotel object to retrieve.</param>
+        /// <returns>Returns a single Hotel object.</returns>
+        // GET: api/Hotels/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Hotel>> GetHotel(int id)
         {
-            
-            return null;
-        }
+            var hotel = await _hotel.GetHotelByID(id);
 
-        // PUT: api/Hotels/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutHotel(int id, Hotel hotel)
-        {
-*//*            if (id != hotel.ID)
-            {
-                return BadRequest();
-            }
-
-            _hotel.Entry(hotel).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!HotelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }*//*
-
-            return NoContent();
-        }
-
-        // POST: api/Hotels
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
-        {
-            _context.Hotels.Add(hotel);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetHotel", new { id = hotel.ID }, hotel);
-        }
-
-        // DELETE: api/Hotels/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Hotel>> DeleteHotel(int id)
-        {
-            var hotel = await _context.Hotels.FindAsync(id);
             if (hotel == null)
             {
                 return NotFound();
             }
 
-            _context.Hotels.Remove(hotel);
-            await _context.SaveChangesAsync();
-
             return hotel;
         }
 
-        private bool HotelExists(int id)
+        /// <summary>
+        /// Updates an existing Hotel object in the Hotels table.
+        /// </summary>
+        /// <param name="id">The ID of the Hotel to update.</param>
+        /// <param name="hotel">The updated information for the Hotel.</param>
+        /// <returns>Nothing.</returns>
+        // PUT: api/Hotels/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHotel(int id, Hotel hotel)
         {
-            return _context.Hotels.Any(e => e.ID == id);
-        }*/
+           if (id != hotel.ID)
+           {
+               return BadRequest();
+           }
+
+            await _hotel.UpdateHotel(id, hotel);
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Deletes a Hotel object from the Hotels table.
+        /// </summary>
+        /// <param name="id">The ID of the Hotel to delete.</param>
+        /// <returns>Nothing.</returns>
+        // DELETE: api/Hotels/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Hotel>> DeleteHotel(int id)
+        {
+            await _hotel.RemoveHotel(id);
+
+            return NoContent();
+        }
     }
 }
