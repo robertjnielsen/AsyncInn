9239b8057e1b685b1c3fe4f883507284af0370aa
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-08 18:59:27 -0700

    Mapped IRoomManager to RoomService for DI.

--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -41,6 +41,7 @@ namespace AsyncInn
 
             // Dependency Injection Mappings
             services.AddTransient<IHotelManager, HotelService>();
+            services.AddTransient<IRoomManager, RoomService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
